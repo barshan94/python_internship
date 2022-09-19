57397bacef5ebab3e83a69cb13526ea34fd286ba
barshan94 <87591860+barshan94@users.noreply.github.com>
2022-09-19 13:56:36 +0600

    Create class4.py

---
+++ class4.py
@@ -0,0 +1,33 @@
+#class 4
+
+#set is an unordered collection of data
+
+'''
+#set
+party={'money',100,'decoration','element',100}
+print(party)
+print(len(party))
+# print(party[1])  #indexing not work
+party.add('false')
+print(party)
+
+'''
+
+
+'''
+var=set()
+print(var)
+var.add('BGB')
+print(var)
+
+a={'do','did','done'}
+b={'do','am','is','are','be'}
+c=a.union(b)
+print(c)
+print(a.intersection(b))
+print(a==b)
+print(a>b)
+print(a!=b)
+print(a<b)
+
+'''
