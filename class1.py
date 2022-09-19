915283df9f99c395a2f61f81f52c1c030974f3a5
barshan94 <87591860+barshan94@users.noreply.github.com>
2022-09-19 12:00:28 +0600

    Create class1.py

---
+++ class1.py
@@ -0,0 +1,22 @@
+#1st class Day 1
+
+#print("BGB")
+
+#print('I am happy')
+
+'''
+a='I'
+b=' am'
+c=" BGB"
+print(a+b+c)
+'''
+
+'''
+a=10
+b=22.3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(type(b))
+'''
