9e18ea2aa2f0ab6bf26655f17ee1c806b80335a2
barshan94 <87591860+barshan94@users.noreply.github.com>
2022-10-11 15:47:48 +0600

    Update class7.py

--- class7.py
+++ class7.py
@@ -1,5 +1,4 @@
 
-# class 7
 
 # three mode - Read(r)/Write(w)/Append(a)
 
@@ -42,7 +41,71 @@ with open("file.txt","r") as file4:
     print(file4.read())
 file4.close()
 
+
+
 """
+
+#Assignment
+
+"""
+#for r+
+
+with open("file.txt","r") as file1:
+   # file1.write("YOYO") #not possible
+    print(file1.read())
+
+# using r+
+
+with open("file.txt","r+") as file2:
+    print(file2.read())
+    file2.write(" YoYo")
+    file2.write(" Honey")
+    file2.write(" Singh")
+    print(file2.read())
+
+"""
+
+#for w+
+
+"""
+with open("file.txt","w") as file1:
+    file1.write("YOYO")
+    # print(file1.read()) #not possible
+
+# using w+
+
+with open("file.txt","w+") as file2:
+    file2.write(" YoYo")
+    file2.write(" Honey")
+    file2.write(" Singh")
+    file2.seek(0) #wont work unless moving the pointer
+    print(file2.read())
+
+
+"""
+
+
+#for a+
+
+"""
+
+with open("file.txt","a") as file1:
+    file1.write("YOYO")
+    # print(file1.read()) #not possible
+
+# using a+
+
+with open("file.txt","a+") as file2:
+    file2.write(" YoYo")
+    file2.write(" Honey")
+    file2.write(" Singh")
+    file2.seek(0) #wont work unless moving the pointer
+    print(file2.read())
+
+
+"""
+
+
 
 #Simple calculator
 #Assignment
@@ -60,5 +123,15 @@ with open("calc.txt","a") as file1:
     file1.close()
 with open("calc.txt","r") as file1:
     print(file1.read())
+
+"""
+
+
+
+"""
+
+import datetime
+x=datetime.datetime.now()
+print(x)
 
 """
